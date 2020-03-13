09064b6632e23e14716173cb2bc5706a824a6ac7
Yuki Yamato <ukitomato.developer@gmail.com>
2020-03-13 10:00:57 +0900

    Update

--- etc/scripts/1.external_app.sh
+++ etc/scripts/1.external_app.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 open https://creativecloud.adobe.com
 open https://www.office.com
+open https://www.eclipse.org/downloads/
+open https://www.apptorium.com/workspaces/beta
+
+
+
--- etc/scripts/3.envs.sh
+++ etc/scripts/3.envs.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
-jenv enable-plugin export && \
+jenv enable-plugin export
 jenv add $(/usr/libexec/java_home -v 1.8) && \
 jenv add $(/usr/libexec/java_home -v 11) && \
-jenv global 11.0.2 && \
+jenv global 11
 plenv install 5.30.1 && \
-plenv global 5.30.1 && \
+plenv global 5.30.1
 rbenv install 2.6.5 && \
-rbenv global 2.6.5 && \
-pyenv install anaconda3-2019.10 && \
+rbenv global 2.6.5
+pyenv install anaconda3-2020.02 && \
 pyenv install 3.8.0 && \
-pyenv global anaconda3-2019.10 && \
-conda init zsh && \
+pyenv global anaconda3-2020.02
+conda init zsh
 nodenv install 10.17.0 && \
 nodenv install 12.13.1 && \
 nodenv install 13.2.0 && \
