35283fdeea8d3101b50aff9c90a15e6c6315240e
Yuki Yamato <ukitomato.developer@gmail.com>
2020-10-30 16:29:58 +0900

    Update 3.envs.sh

--- etc/scripts/3.envs.sh
+++ etc/scripts/3.envs.sh
@@ -5,7 +5,7 @@ jenv add $(/usr/libexec/java_home -v 11) && \
 jenv add $(/usr/libexec/java_home -v 15) && \
 jenv global 15 && \
 plenv install 5.32.0 && \
-plenv global 5.30.1
+plenv global 5.32.0
 rbenv install 2.7.2 && \
 rbenv global 2.7.2
 pyenv install 3.9.0 && \
@@ -15,5 +15,5 @@ nodenv install 12.19.0 && \
 nodenv install 14.14.0 && \
 nodenv install 15.0.1 && \
 nodenv global 14.14.0 && \
-goenv install 1.15 && \
+goenv install 1.15.3 && \
 goenv global 1.15.3
